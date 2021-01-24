cc32c772242170a24673e57cac5b4537fd54ffc9
Emil Emilov Dimitrov <emdimitrov.96@gmail.com>
2021-01-24 20:33:10 +0200

    Initial commit

---
+++ src/test/java/com/mentormate/csproject/CsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentormate.csproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
